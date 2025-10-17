bc605a967631b8338eed71d10f22acf7590d31a9
iot-capitan <o.almrayat@psut.edu.jo>
2025-10-17 22:35:30 +0300

    created product.p

--- CRUD/products.py
+++ CRUD/products.py
@@ -15,4 +15,38 @@ products = [
 #Add all the REST API end-points here
 #
 
+@app.route("/products", methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+@app.route("/products/<id>", methods=['GET'])
+def get_product(id):
+    id = int(id)
+    for product in products:
+        if product['id'] == id:
+            return jsonify(product)
+    return jsonify({'error': 'Product not found'}), 404
+
+@app.route("/products", methods=['POST'])
+def add_product():
+    new_product = request.get_json()
+    products.append(new_product)
+    return jsonify(new_product), 201
+
+@app.route("/products/<id>", methods=['PUT'])
+def update_product(id):
+    id = int(id)
+    update_product = json.loads(request.data)
+    product = [x for x in products if x['id'] == id][0]
+    for key, value in update_product.items():
+        product[key] = value
+    return '', 204
+
+@app.route("/products/<id>", methods=['DELETE'])
+def remove_product(id):
+    id = int(id)
+    product = [x for x in products if x['id'] == id][0]
+    products.remove(product)
+    return '', 204
+
 app.run(port=5000,debug=True)
